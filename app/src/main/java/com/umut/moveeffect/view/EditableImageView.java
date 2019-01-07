08b970ac53eba151d70e1e638dbea660f38f2840
umutyusuf <umutyusuft@gmail.com>
2019-01-07 21:49:52 +0300

    performance enhancements

--- app/src/main/java/com/umut/moveeffect/view/EditableImageView.java
+++ app/src/main/java/com/umut/moveeffect/view/EditableImageView.java
@@ -22,9 +22,6 @@ import com.umut.moveeffect.util.BitmapUtils;
 import com.umut.moveeffect.util.Constants;
 import com.umut.moveeffect.util.PointUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class EditableImageView extends AppCompatImageView {
 
     private static final int LINE_COLOR = Color.GRAY;
@@ -40,6 +37,8 @@ public class EditableImageView extends AppCompatImageView {
     @NonNull
     private final Paint croppedBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     @NonNull
+    private final Paint copyBitmapPath = new Paint(Paint.ANTI_ALIAS_FLAG);
+    @NonNull
     private final PointF initialPoint = new PointF(-Constants.INITIAL_CIRCLE_RADIUS, -Constants.INITIAL_CIRCLE_RADIUS);
     @NonNull
     private final PointF lastPoint = new PointF();
@@ -48,22 +47,21 @@ public class EditableImageView extends AppCompatImageView {
     @NonNull
     private final PointF lastDragPoint = new PointF();
     @NonNull
-    private final List<PointF> offsetList = new ArrayList<>();
-    @NonNull
     private final Rect selectionRect = new Rect();
+    @NonNull
+    private final RectF drawRect = new RectF();
 
     @MarkState
     private int state;
     @Nullable
     private Bitmap croppedAreaBitmap;
-    @Nullable
-    private Canvas moveBitmapCanvas;
-    @Nullable
-    private Bitmap moveBitmap;
+    private float diffStepX;
+    private float diffStepY;
 
     @Nullable
     private SelectionStateListener listener;
     private int repCount;
+    private int markCount;
 
     public EditableImageView(Context context) {
         this(context, null);
@@ -83,23 +81,17 @@ public class EditableImageView extends AppCompatImageView {
         circlePaint.setStyle(Paint.Style.FILL);
         circlePaint.setColor(CIRCLE_COLOR);
 
-        croppedBitmapPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
+        croppedBitmapPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        copyBitmapPath.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        copyBitmapPath.setAlpha(255);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (croppedAreaBitmap != null && moveBitmap != null) {
-            canvas.drawBitmap(moveBitmap, 0, 0, null);
-        }
         canvas.drawCircle(initialPoint.x, initialPoint.y, Constants.INITIAL_CIRCLE_RADIUS, circlePaint);
         canvas.drawPath(selectionPath, linePaint);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        populateMoveBitmap();
+        drawOverlays(canvas);
     }
 
     @Override
@@ -135,61 +127,22 @@ public class EditableImageView extends AppCompatImageView {
 
     public void updateAlpha(int alpha) {
         croppedBitmapPaint.setAlpha(alpha);
-        prepareMoveCanvas();
         invalidate();
     }
 
     public void updateRepCount(int nRepCount) {
         this.repCount = nRepCount;
         computeOffsetList();
-        invalidate();
     }
 
     @Nullable
     public Bitmap getDrawnBitmap() {
         final Bitmap output = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         final Canvas canvas = new Canvas(output);
-        if (moveBitmap != null) {
-            canvas.drawBitmap(((BitmapDrawable) getDrawable()).getBitmap(), 0, 0, null);
-            canvas.drawBitmap(moveBitmap, 0, 0, null);
+        if (drawOverlays(canvas)) {
             return output;
-        } else {
-            return null;
         }
-    }
-
-    private void prepareMoveCanvas() {
-        eraseMoveBitmap();
-        if (croppedAreaBitmap != null) {
-            drawMoveCanvas(croppedAreaBitmap);
-        }
-    }
-
-    private void drawMoveCanvas(@NonNull Bitmap croppedAreaBitmap) {
-        if (moveBitmap == null) {
-            populateMoveBitmap();
-        }
-        if (moveBitmapCanvas == null) {
-            moveBitmapCanvas = new Canvas(moveBitmap);
-        }
-        if (offsetList.isEmpty()) {
-            return;
-        }
-        moveBitmapCanvas.save();
-        moveBitmapCanvas.drawBitmap(croppedAreaBitmap, null, selectionRect, null);
-        RectF drawRect = new RectF(selectionRect);
-        for (PointF p : offsetList) {
-            drawRect.set(selectionRect);
-            drawRect.offset(p.x, p.y);
-            if (!moveBitmapCanvas.quickReject(drawRect, Canvas.EdgeType.AA)) {
-                moveBitmapCanvas.drawBitmap(croppedAreaBitmap, null, drawRect, croppedBitmapPaint);
-            }
-        }
-        moveBitmapCanvas.restore();
-    }
-
-    private void populateMoveBitmap() {
-        moveBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        return null;
     }
 
     private void onActionDown(@NonNull PointF point) {
@@ -217,6 +170,7 @@ public class EditableImageView extends AppCompatImageView {
         clearDrawings();
         initialPoint.set(point);
         lastPoint.set(point);
+        markCount = 0;
         moveToPoint(point);
         invalidate();
     }
@@ -226,9 +180,10 @@ public class EditableImageView extends AppCompatImageView {
             croppedAreaBitmap.recycle();
         }
         croppedAreaBitmap = null;
-        moveBitmapCanvas = null;
-        offsetList.clear();
-        eraseMoveBitmap();
+        lastDragPoint.set(0, 0);
+        grabPoint.set(0, 0);
+        diffStepX = 0;
+        diffStepY = 0;
         rewind();
         onAreaSelectReleased();
     }
@@ -250,28 +205,19 @@ public class EditableImageView extends AppCompatImageView {
             computeOffsetList();
         } else {
             state = MarkState.MARKER_MOVE;
+            markCount++;
             if (!computeIntersect(point)) {
                 lastPoint.set(point);
                 addToPath(point);
             }
+            invalidate();
         }
-        invalidate();
     }
 
     private void computeOffsetList() {
-        offsetList.clear();
-        final float diffStepX = (lastDragPoint.x - grabPoint.x) / repCount;
-        final float diffStepY = (lastDragPoint.y - grabPoint.y) / repCount;
-        for (int i = 1; i <= repCount; i++) {
-            offsetList.add(new PointF(diffStepX * i, diffStepY * i));
-        }
-        prepareMoveCanvas();
-    }
-
-    private void eraseMoveBitmap() {
-        if (moveBitmap != null) {
-            moveBitmap.eraseColor(Color.TRANSPARENT);
-        }
+        diffStepX = (lastDragPoint.x - grabPoint.x) / repCount;
+        diffStepY = (lastDragPoint.y - grabPoint.y) / repCount;
+        invalidate();
     }
 
     private void valuatePathWithPoint(@NonNull PointF point) {
@@ -315,17 +261,40 @@ public class EditableImageView extends AppCompatImageView {
                 copyPath.quadTo(nPoint.x, nPoint.y, initialPoint.x, initialPoint.y);
                 copyPath.close();
                 if (PointUtils.computePathArea(copyPath) > Constants.MIN_ALLOWED_SNAP_AREA) {
-                    qAddToPath(nPoint, initialPoint);
-                    closePath();
-                    onAreaSelected();
-                    this.selectionRect.set(PointUtils.getRectOfPath(selectionPath));
-                    croppedAreaBitmap =
-                            BitmapUtils.getCroppedBitmap(
-                                    ((BitmapDrawable) getDrawable()).getBitmap(),
-                                    selectionPath);
+                    performCrop(nPoint);
                     return true;
                 }
             }
+        }
+        if (markCount > Constants.INTERSECT_DEVIATION && PointUtils.lazyMatch(initialPoint, nPoint)) {
+            performCrop(nPoint);
+            return true;
+        }
+        return false;
+    }
+
+    private void performCrop(@NonNull PointF nPoint) {
+        qAddToPath(nPoint, initialPoint);
+        closePath();
+        onAreaSelected();
+        this.selectionRect.set(PointUtils.getRectOfPath(selectionPath));
+        croppedAreaBitmap =
+                BitmapUtils.getCroppedBitmap(
+                        ((BitmapDrawable) getDrawable()).getBitmap(),
+                        selectionPath);
+    }
+
+    private boolean drawOverlays(Canvas canvas) {
+        if (croppedAreaBitmap != null && (diffStepX != 0 || diffStepY != 0)) {
+            drawRect.set(selectionRect);
+            for (int i = 0; i < repCount; i++) {
+                drawRect.offset(diffStepX, diffStepY);
+                if (!canvas.quickReject(drawRect, Canvas.EdgeType.AA)) {
+                    canvas.drawBitmap(croppedAreaBitmap, null, drawRect, croppedBitmapPaint);
+                }
+            }
+            canvas.drawBitmap(croppedAreaBitmap, null, selectionRect, copyBitmapPath);
+            return true;
         }
         return false;
     }
